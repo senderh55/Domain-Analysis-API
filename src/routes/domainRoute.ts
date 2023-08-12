4b7f4851184eba3b80af107a431986203a265366
Sender Hodik <senderh55@gmail.com>
2023-08-12 17:43:01 +0300

    store log requset implemented

--- src/routes/domainRoute.ts
+++ src/routes/domainRoute.ts
@@ -5,6 +5,8 @@ import {
 } from "../middlewares/domainValidationMiddleware";
 const router = express.Router();
 import * as domainController from "../controllers/domainController";
+import { logRequest } from "../middlewares/loggingMiddleware"; // Import the middleware
+router.use(logRequest);
 
 // FIXME: add auth middlewares to routes
 router.get("/domainAnalysis/:domainName", domainController.getDomainInfo);
