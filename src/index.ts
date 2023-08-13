9ba23d161ccd67885ee220ff021cd809ea866f03
Sender Hodik <senderh55@gmail.com>
2023-08-13 11:29:53 +0300

    api limiter updated'

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,7 @@ const express = require("express");
 const domainRouter = require("./routes/domainRoute");
 const cors = require("cors");
 import { startConsumer } from "./controllers/domainController";
-import swaggerUi from "swagger-ui-express";
-import swaggerSpec from "./swagger";
+
 const {
   startDomainAnalysisScheduler,
 } = require("./services/schedulingService");
@@ -19,12 +18,9 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(helmet()); // Helmet for security
-
+app.use(domainRouter);
 app.use(limiter);
 app.use(logRequest);
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use(domainRouter);
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
--- src/middlewares/rateLimitMiddleware.ts
+++ src/middlewares/rateLimitMiddleware.ts
@@ -2,6 +2,6 @@ import rateLimit from "express-rate-limit";
 /**  Rate limiter middleware **/
 export const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: 50, // limit each IP to 5 requests per windowMs
+  max: 5, // limit each IP to 5 requests per windowMs
   message: "Too many requests, please try again later.",
 });
